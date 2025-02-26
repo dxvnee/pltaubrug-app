23cd0cbde3013c41bd643e57bcb4b700ebda0f03
dxvnee <egiadaramuli81@gmail.com>
2025-02-26 07:27:34 +0700

    Perbaikan Viewmodel

---
+++ app/src/main/java/org/d3if3121/absenubrug/ui/viewmodel/MahasiswaListViewModel.kt
@@ -0,0 +1,149 @@
+package org.d3if3121.absenubrug.ui.viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.d3if3121.absenubrug.data.model.Absen
+import org.d3if3121.absenubrug.data.model.Mahasiswa
+import org.d3if3121.absenubrug.data.model.MahasiswaEdit
+import org.d3if3121.absenubrug.data.model.MahasiswaLogin
+import org.d3if3121.absenubrug.data.model.Response
+import org.d3if3121.absenubrug.data.repository.interfaces.AddMahasiswaResponse
+import org.d3if3121.absenubrug.data.repository.interfaces.AddAbsenResponse
+import org.d3if3121.absenubrug.data.repository.interfaces.LoginResponse
+import org.d3if3121.absenubrug.data.repository.interfaces.MahasiswaListInterface
+import org.d3if3121.absenubrug.data.repository.interfaces.AbsenListResponse
+import javax.inject.Inject
+import kotlin.math.log
+
+@HiltViewModel
+class MahasiswaListViewModel @Inject constructor(
+    private val repo: MahasiswaListInterface
+): ViewModel() {
+    var absenListResponse by mutableStateOf<AbsenListResponse>(Response.Loading)
+        private set
+
+    var absenList by mutableStateOf<List<Absen>>(emptyList())
+        private set
+
+    var addMahasiswaResponse by mutableStateOf<AddMahasiswaResponse>(Response.Loading)
+        private set
+    var addAbsenResponse by mutableStateOf<AddAbsenResponse>(Response.Loading)
+        private set
+
+    var loginResponse by mutableStateOf<LoginResponse>(Response.Loading)
+        private set
+
+    var user by mutableStateOf(Mahasiswa())
+        private set
+
+    var tanggal by mutableStateOf("")
+        private set
+
+    var tanggalSeharusnya by mutableStateOf("")
+        private set
+
+    var keteranganhome by mutableStateOf("")
+        private set
+    var keteranganhome2 by mutableStateOf("")
+        private set
+
+    var jamhome by mutableStateOf("")
+        private set
+    var jamhome2 by mutableStateOf("")
+        private set
+
+    var currentAbsen by mutableStateOf(Absen())
+        private set
+
+    var loading by mutableStateOf(false)
+        private set
+
+
+    fun getAbsenList(user: Mahasiswa) = viewModelScope.launch {
+        repo.getAbsenList(user).collect() {
+            absenListResponse = it
+            Log.d("hew", it.toString())
+
+        }
+    }
+
+    fun addUser(mahasiswa: Mahasiswa) {
+        user = mahasiswa
+    }
+    fun changeLoading(input: Boolean){
+        loading = input
+    }
+
+    fun changeTanggal(selectedDate: String){
+        tanggal = selectedDate
+    }
+    fun changeAbsenListResponse(){
+        absenListResponse = Response.Loading
+    }
+    fun changeTanggalSeharusnya(selectedDate: String){
+        tanggalSeharusnya = selectedDate
+        Log.d("tanggalSeharusnya", tanggalSeharusnya)
+
+    }
+
+    fun changeAbsen(input: Absen){
+        currentAbsen = input
+    }
+
+    fun changeList(absen: List<Absen>){
+        absenList = absen
+    }
+
+    fun changeKeteranganHome(input: String, input2: String){
+        keteranganhome = input
+        keteranganhome2 = input2
+    }
+
+
+    fun changeJamHome(input: String, input2: String){
+        jamhome = input
+        jamhome2 = input2
+    }
+
+
+    fun addMahasiswa(mahasiswa: Mahasiswa) = viewModelScope.launch {
+        addMahasiswaResponse = repo.addMahasiswa(mahasiswa)
+    }
+
+    fun addAbsen(absen: Absen) = viewModelScope.launch {
+        changeLoading(true)
+        addAbsenResponse = repo.addAbsen(absen)
+    }
+    fun addAbsenPulang(absen: Absen) = viewModelScope.launch {
+        changeLoading(true)
+        addAbsenResponse = repo.addAbsenPulang(absen)
+    }
+
+    fun addAbsenResponseReset() {
+        addAbsenResponse = Response.Loading
+    }
+
+    fun loginResponseReset() {
+        loginResponse = Response.Loading
+        user = Mahasiswa()
+    }
+
+    fun loginMahasiswa(response: MahasiswaLogin) = viewModelScope.launch {
+        changeLoading(true)
+        loginResponse = repo.loginMahasiswa(response.nim, response.password)
+    }
+
+
+
+
+}
