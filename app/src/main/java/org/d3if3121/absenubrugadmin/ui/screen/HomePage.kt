4e691127b024c40b8b39d69fbe0cd4655facb806
dxvnee <egiadaramuli81@gmail.com>
2025-03-11 14:14:44 +0700

    commit insttal

--- app/src/main/java/org/d3if3121/absenubrugadmin/ui/screen/HomePage.kt
+++ app/src/main/java/org/d3if3121/absenubrugadmin/ui/screen/HomePage.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.util.Log
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -34,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -160,7 +162,14 @@ fun ProjectListHome(
         Column (
             modifier = Modifier.padding(17.dp).fillMaxWidth().fillMaxHeight()
         ){
-            Log.d("absenlist", viewmodel.absenList.toString())
+            Text(
+                text = "Input ke Spreadsheet!",
+                color = Warna.MerahNormal,
+                fontSize = 12.sp,
+                modifier = Modifier.clickable {
+
+                }
+            )
 
             Button(
                 onClick = {
